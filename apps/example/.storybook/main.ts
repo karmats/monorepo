faf7cc4bfedc5f8904b7d825f15e3601e9f1a2db
Mats Roshauw <mats.roshauw@appva.com>
2023-06-07 13:22:03 +0200

    Upgarde storybook

--- apps/example/.storybook/main.ts
+++ apps/example/.storybook/main.ts
@@ -1,18 +1,13 @@
 import type { StorybookConfig } from "@storybook/vue3-vite";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    '@storybook/addon-a11y',
-  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", '@storybook/addon-a11y', "@storybook/addon-mdx-gfm"],
   framework: {
     name: "@storybook/vue3-vite",
-    options: {},
+    options: {}
   },
   docs: {
-    autodocs: "tag",
-  },
+    autodocs: "tag"
+  }
 };
 export default config;
