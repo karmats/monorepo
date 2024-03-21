3ee80faa57b73634f15780cbe458bb42090c43e0
Mats Roshauw <mats.roshauw@appva.com>
2024-03-21 09:31:03 +0100

    Login username/password

--- packages/material/src/index.js
+++ packages/material/src/index.js
@@ -1,5 +1,7 @@
 import "@material/web/button/filled-button.js";
 import "@material/web/button/text-button.js";
+import "@material/web/iconbutton/icon-button.js";
+import "@material/web/dialog/dialog.js";
 import "@material/web/textfield/outlined-text-field.js";
 import "@material/web/elevation/elevation.js";
 
